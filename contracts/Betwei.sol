453d11922d41568f69cc33cc4a3aeea0c4bb9e73
Julian Murphy <njmdistrisoft@gmail.com>
2022-08-27 16:52:24 -0300

    use rinkeby coordinator

--- contracts/Betwei.sol
+++ contracts/Betwei.sol
@@ -10,8 +10,8 @@ contract Betwei is VRFConsumerBaseV2 {
 
   uint64 s_subscriptionId;
 
-  address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
-  bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+  address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+  bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
 
   uint32 callbackGasLimit = 100000;
 
