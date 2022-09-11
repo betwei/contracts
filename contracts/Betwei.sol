b75ac2829f054ddd9f5f8275ea3edb172e52ce0f
Julian Murphy <njmdistrisoft@gmail.com>
2022-09-11 02:02:30 -0300

    comments

--- contracts/Betwei.sol
+++ contracts/Betwei.sol
@@ -14,7 +14,7 @@ contract Betwei is VRFConsumerBaseV2 {
 
   uint64 immutable s_subscriptionId;
 
-  // Rinkeby address and keyhash Chainlink VRF
+  // @notice Rinkeby address and keyhash Chainlink VRF
   address immutable vrfCoordinator; // = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
   bytes32 immutable keyHash; // = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
 
@@ -101,22 +101,34 @@ contract Betwei is VRFConsumerBaseV2 {
    * Manage game
    */
 
-  /**
-   * Create new game
-   * duration param -> max players
-   * return gameId
-   * TODO: amount parameter
-   */
+  // @notice Create new game, raffle a winner
+  // @dev create a new Game, the type of the game is 0 (RANDOMWINNER).
+  // @param _duration max players
+  // @param _description description for humans
+  // @return uint256
   function createSimpleNewGame(uint256 _duration, string memory _description) public payable hasAmount returns(uint256) {
       return _createNewGame(GameType.RANDOMWINNER, _duration, _description);
   }
 
+  // @notice Create new game, draw a NFT
+  // @notice Require approve transferFrom for this Betwei address
+  // @dev create a new Game, the type of the game is 1 (RANDOM_NFT_WINNER).
+  // @param _nftContract external contract address ERC721
+  // @param _tokenId token id into ERC721 contract
+  // @param _duration max players
+  // @param _description description for humans
+  // @return uint256
   function createRandomNFTGame(IERC721 _nftContract, uint256 _tokenId, uint256 _duration, string memory _description) public returns(uint256 _gameId) {
     NFTGameRandom memory newNft = NFTGameRandom(_nftContract, _tokenId);
     _gameId = _createNewGame(GameType.RANDOM_NFT_WINNER, _duration, _description);
     nftGame[_gameId] = newNft;
   }
 
+  // @dev internal function to create a new game.
+  // @param _type Type of game
+  // @param _duration Max players
+  // @param _description User description for the game
+  // @returns uint256 Id the game
   function _createNewGame(GameType _type, uint256 _duration, string memory _description) internal returns (uint256) {
     uint256 newIndex = indexedGames.length;
     emit NewGameCreated(newIndex);
@@ -139,6 +151,10 @@ contract Betwei is VRFConsumerBaseV2 {
     return newIndex;
   }
 
+  // @notice enroll to the game gameId
+  // @dev only require neededAmount for RANDOMWINNER GameType
+  // @param gameId
+  // @return bool
   function enrollToGame(uint256 gameId)
     external
     payable
@@ -149,25 +165,31 @@ contract Betwei is VRFConsumerBaseV2 {
     Game storage game = indexedGames[gameId];
     require(game.gameType == GameType.RANDOM_NFT_WINNER || game.neededAmount <= msg.value, "The amount required should be greather or equal");
     emit EnrolledToGame(gameId, msg.sender);
+    // add member to the game
     game.members.push(payable(address(msg.sender)));
+    // add human readable description for the site
     games[msg.sender].push(
         string(abi.encodePacked(gameId.toString(), '-', game.description))
     );
+    // max players? close the game
     if (game.duration <= game.members.length) {
       game.status = GameStatus.CLOSED;
     }
-    //game.playersBalance[msg.sender] += msg.value;
+    // add amount to player balance and game balance
     playerBalanceByGame[gameId][msg.sender] += msg.value;
     game.balance += msg.value;
 
     return true;
   }
 
+  // @notice coun users enrolled to game
+  // @param gameId
   function usersEnrolled(uint256 gameId) external view gameExists(gameId) returns(uint256) {
     Game storage game = indexedGames[gameId];
     return game.members.length;
   }
 
+  // @notice close enroll
   function closeGame(uint256 gameId) external gameExists(gameId) canManageGame(gameId) returns(bool) {
     Game memory game = indexedGames[gameId];
     if(
@@ -182,6 +204,7 @@ contract Betwei is VRFConsumerBaseV2 {
     return true;
   }
 
+  // @notice start the game and wait for calculate winner
   function startGame(uint256 gameId) external gameExists(gameId) canManageGame(gameId) {
     Game storage game = indexedGames[gameId];
     require(game.status == GameStatus.CLOSED, 'The game not is closed');
@@ -192,6 +215,7 @@ contract Betwei is VRFConsumerBaseV2 {
 
   }
 
+  // @dev send request to Chainlink VRF
   function _calculatingWinner(Game memory game) internal  {
     require(game.status == GameStatus.CALCULATING, "You aren't at that stage yet!");
 
@@ -222,22 +246,24 @@ contract Betwei is VRFConsumerBaseV2 {
   }
 
   function _selectWinner(uint256 requestId, uint256[] memory randomWords) internal {
+    // get game id
     uint256 gameIndex = requests[requestId];
+    // get game
     Game storage game = indexedGames[gameIndex];
     require(game.status == GameStatus.CALCULATING, "Game no have status Calculating");
     emit FinishGame(game.gameId);
+    // set new status and random solution
     game.status = GameStatus.FINISHED;
     game.solution = randomWords[0];
+    // calculate winner
     uint256 winnerIndex = game.solution % game.members.length;
     address winnerAddress = game.members[winnerIndex];
-    // only 1 winner
+    // temporal: only 1 winner
     game.winnersIndexed.push(winnerAddress);
     winnersByGame[gameIndex][winnerAddress] = true;
   }
 
-  /**
-   * Withdraw function
-   */
+  // @notice withdraw award. in the case RANDOM_NFT_WINNER, transfer owner token
   function withdrawGame(uint256 _gameId) external gameExists(_gameId) returns(bool) {
     Game memory game = indexedGames[_gameId];
     require(game.balance != 0, "Game finished, balance 0");
@@ -250,7 +276,7 @@ contract Betwei is VRFConsumerBaseV2 {
     // save game
     indexedGames[_gameId] = game;
 
-    if (uint(game.gameType) == 0) {
+    if (game.gameType == GameType.RANDOMWINNER) {
         // transfer all game balance
         (bool success,) = payable(msg.sender).call{value: balanceGame}("");
         require(success, "Transfer amount fail");
@@ -258,9 +284,10 @@ contract Betwei is VRFConsumerBaseV2 {
         return true;
     }
 
-    if (uint(game.gameType) == 1) {
+    if (game.gameType == GameType.RANDOM_NFT_WINNER) {
         NFTGameRandom memory _nftGame = nftGame[_gameId];
         IERC721 _nftContract = _nftGame.nftContract;
+        // transfer from owner game to winner
         _nftContract.transferFrom(game.owner, msg.sender, _nftGame.tokenId);
         return true;
     }
@@ -288,6 +315,7 @@ contract Betwei is VRFConsumerBaseV2 {
       return games[_player];
   }
 
+  // @notice get game info
   function viewGame(uint256 _gameId)
     external
     view
